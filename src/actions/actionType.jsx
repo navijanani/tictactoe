a8e48d2a84a2dd1c2ff9250b5f605dc2d8827e53
navijanani <navaranjitham345@gmail.com>
2024-12-12 17:18:07 +0530

    nav bar is deleted

--- src/actions/actionType.jsx
+++ src/actions/actionType.jsx
@@ -1,6 +1,6 @@
 export const SET_PLAYERS = 'SET_PLAYERS'; // Set player names
 export const SET_SYMBOLS = 'SET_SYMBOLS'; // Set symbols ('X' or 'O')
 export const MAKE_MOVE = 'MAKE_MOVE'; // Make a move on the board
-export const RESET_GAME = 'RESET_GAME'; // Reset the game
+export const RESET_GAME = 'RESET_GAME'; // Reset the game and play gain
 export const SET_WINNER = 'SET_WINNER'; // Set the game winner
-export const RESET_GAME_ALL = 'RESET_GAME_ALL';
+export const RESET_GAME_ALL = 'RESET_GAME_ALL';// Reset everything to start new game
--- src/actions/gameActions.jsx
+++ src/actions/gameActions.jsx
@@ -12,30 +12,24 @@ export const setPlayers = (players) => ({
   payload: players,
 });
 
-// Action to set symbols for players
 export const setSymbols = (symbol) => ({
   type: SET_SYMBOLS,
   payload: symbol,
 });
 
-// Action for a move on the board
 export const makeMove = (index) => ({
   type: MAKE_MOVE,
   payload: index,
 });
 
-// Action to reset the game
 export const resetGame = () => ({
   type: RESET_GAME,
 });
 
-// Action to set winner
 export const setWinner = (winner) => ({
   type: SET_WINNER,
   payload: winner,
 });
-// gameActions.js
-// Action to reset everything
 export const resetGameAll = () => ({
   type: RESET_GAME_ALL,
 });
